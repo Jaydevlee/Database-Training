316c9ebaced8491c80728611d6ec2cb776d346ff
Jaydevlee <>
2025-11-03 09:35:45 +0900

    주석 추가

--- 다중행 함수/다중행되새김문제.sql
+++ 다중행 함수/다중행되새김문제.sql
@@ -15,11 +15,12 @@ SELECT JOB,
       HAVING COUNT(JOB) >= 3; -- 그룹화한 조건은 WHERE을 사용할 수 없다.
       
 --3
-SELECT TO_CHAR(HIREDATE, 'YYYY') AS HIRE_YEAR,
+SELECT TO_CHAR(HIREDATE, 'YYYY') AS HIRE_YEAR, -- 별칭으로 GROUP BY를 적용할 수 없다.
         DEPTNO,
         COUNT(*) AS CNT
         FROM EMP
       GROUP BY TO_CHAR(HIREDATE, 'YYYY'), DEPTNO;
+      /* 별칭으로 그룹화를 하려면 서브 쿼리를 활용 */
         
 -- 4
 SELECT NVL2(COMM, 'O', 'X') AS EXIST_COMM,
